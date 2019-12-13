74efba89bbf0c80dda2cfa0ce884e666a9730073
Lee Hudson <lee.hudson@diamond.ac.uk>
2019-12-13 08:20:36 +0000

    Tidied up make

--- scripts/updateFromXmlDir.py
+++ scripts/updateFromXmlDir.py
@@ -7,6 +7,8 @@ stdout = Popen('ls ../xml | grep .xml', shell=True, stdout=PIPE).stdout
 
 xmlFiles = stdout.read().split()
 
+os.system(f'rm ../convert.err')
+
 for camera in xmlFiles:
     print(f'Converting {camera.decode()}')
-    os.system(f'dls-python3 makeDbEdl.py ../xml/{camera.decode()} ')
+    os.system(f'dls-python3 makeDbEdl.py ../xml/{camera.decode()} &>> ../convert.err')
